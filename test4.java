1e56f6e8b7a05d9d51b1b47d8cd055a37d1b470b
HARSH CHOUDHARY <harshmnnit2006@gmail.com>
2025-12-21 16:28:23 +0530

    Update test4.java

--- test4.java
+++ test4.java
@@ -3,5 +3,8 @@ public class Solution{
   public static void main(String args[]){
     System.out.println("Hello world");
     int a=5;
+    int b=7;
+    int c=a+b;
+    System.out.println(a+b+c);
   }
 }
